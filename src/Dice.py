092095bef80a3ece218d09596feebb8982e506d8
Benjamin Yliniemi <benjamin.yliniemi@snhu.edu>
2020-07-08 21:21:17 -0400

    Dice Class completed

---
+++ src/Dice.py
@@ -0,0 +1,27 @@
+import random
+
+class Dice:
+    """
+    Dice class for rolling in the game
+    Keyword arguments:
+        sides -- the number of sides on the dice
+    Raises: 
+        Value Error - Sides must be > 1
+    """
+    def __init__(self, sides=6): 
+        if(sides <= 1):  
+            raise ValueError("Dice must have sides")
+        self.sides = sides
+
+    def roll(self):
+        """
+        Roll the dice
+
+        Returns:
+            A random number between 1 and the number of sides on the dice
+        """
+        return random.randint(1,self.sides)
+
+if __name__ == "__main__":
+    dice = Dice()
+    print(dice.roll())
